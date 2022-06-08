a5af4f330f2ebc05914df1571c4e07d4041c948b
Guilherme Silva Reis <guilherme-sr@juceg.local>
2022-06-08 16:21:14 -0300

    stylingForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewTransferComponent } from './newTransfer/newTransfer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTransferComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/newTransfer/newTransfer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app-newTransfer',
+    templateUrl:'./newTransfer.component.html',
+    styleUrls:['./newTransfer.component.scss']
+})
+export class NewTransferComponent{
+
+}
